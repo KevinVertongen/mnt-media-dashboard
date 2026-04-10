992eff66b221385803aaa73de830b9ffffb9cede
Kevin Vertongen <kevin.vertongen@gmail.com>
2026-04-10 13:14:00 +0200

    Add Gradle, configure SQLite

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.0.5"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "be.mnt"
+version = "0.0.1"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.hibernate:hibernate-community-dialects:7.3.0.Final")
+    runtimeOnly("org.xerial:sqlite-jdbc")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
