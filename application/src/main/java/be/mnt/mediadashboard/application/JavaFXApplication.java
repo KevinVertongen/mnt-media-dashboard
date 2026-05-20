346d4c06f4d15069f85659433d55af32e48682f5
Kevin Vertongen <kevin.vertongen@gmail.com>
2026-05-20 09:48:52 +0200

    Pass commandline arguments to Spring ApplicationContext.

--- application/src/main/java/be/mnt/mediadashboard/application/JavaFXApplication.java
+++ application/src/main/java/be/mnt/mediadashboard/application/JavaFXApplication.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.List;
+
 public class JavaFXApplication extends Application {
 
     private static final Logger log = LoggerFactory.getLogger(JavaFXApplication.class);
@@ -17,7 +19,13 @@ public class JavaFXApplication extends Application {
 
     @Override
     public void init() {
-        applicationContext = new SpringApplicationBuilder(SpringApplication.class).run();
+        applicationContext = new SpringApplicationBuilder(SpringApplication.class).run(getArguments());
+    }
+
+    private String[] getArguments() {
+        final List<String> argumentList = getParameters().getRaw();
+        final int argumentSize = argumentList.size();
+        return argumentList.toArray(new String[argumentSize]);
     }
 
     @Override
@@ -28,7 +36,7 @@ public class JavaFXApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        log.info("dashboardStage: " + primaryStage);
+        log.info("primaryStage: " + primaryStage);
         this.dashboardStage = new DashboardStage(primaryStage);
         this.dashboardStage.show();
     }
